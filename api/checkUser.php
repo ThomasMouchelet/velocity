00041ec7e3c4836b476a8ed7468696085505f208
thomasM <mouchelet.thomas@gmail.com>
2019-02-20 15:57:08 +0100

    login post ajax + show map hide loginForm

--- api/checkUser.php
+++ api/checkUser.php
@@ -3,6 +3,10 @@ header('Access-Control-Allow-Origin: *');
 
 $user = [];
 
+// Create database in phpMyadmin with user table (id, username, password)
+// Connect to database
+// fetch user in database with SQL request
+
 if( $_POST["username"] == "admin" && $_POST["password"] == "admin"){
     $user = [
         "username" => "admin"
